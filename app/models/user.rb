d129756f365a3330bd12bc830887d7be669813cd
kbjude <kbjude@gmail.com>
2020-05-19 01:10:31 +0300

    User WIP password pending

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :user_name, presence: true, length: { minimum: 2, maximum: 50 }
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: { minimum: 8, maximum: 70 }
+    validates :password_confirmation, length: { minimum: 8, maximum: 70 }
 end
--- db/migrate/20200518194841_create_users.rb
+++ db/migrate/20200518194841_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :user_name
-      t.string :email
-      t.string :password
-      t.string :password_confirmation
+      t.string :user_name,  :null => false, :limit => 50
+      t.string :email,      :null => false 
+      t.string :password    :password
+      t.string :password_confirmation :password
 
       t.timestamps
     end
