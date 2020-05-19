3ec48ba3bc44cac879559288d9ab75b02ef4db8e
kbjude <kbjude@gmail.com>
2020-05-19 10:09:43 +0300

    Post and its migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_225355) do
+ActiveRecord::Schema.define(version: 2020_05_19_065654) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title", null: false
+    t.text "body", limit: 10, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
